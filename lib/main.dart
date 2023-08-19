4504a801fe17613808eb2de62508f0baab4c74bd
Matheus Dario <dario.matheus97@gmail.com>
2023-08-18 21:28:53 -0300

    add icons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class VidenteVirtualPage extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color.fromRGBO(51, 43, 37, 1.0),
         appBar: AppBar(
-          title: Text('Vidente Virtual'),
+          title: Center(child: Text('Vidente Virtual')),
           backgroundColor: Colors.brown[800],
         ),
         body: Vidente(),
